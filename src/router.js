9bb9f24e59d147cf9a051e5938f29cdbd9fd2f57
x-ray <princesswar721@gmail.com>
2023-06-20 18:25:11 +0800

    fix: change the router mode

--- src/router.js
+++ src/router.js
@@ -1,6 +1,6 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes: [
     {
       path: "/",
